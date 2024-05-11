7201aac8be18ecf6bf10d3df03967816e15bd8d7
Galower1 <ariosgalower@gmail.com>
2024-05-11 16:13:21 -0400

    bind expressions to hotkeys

--- src/routes/vtube/+page.svelte
+++ src/routes/vtube/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  import { Label } from "$lib/components/ui/label";
   import * as Card from "$lib/components/ui/card";
   import { Button } from "$lib/components/ui/button";
   import { toast } from "svelte-sonner";
+  import { Input } from "$lib/components/ui/input";
+  import { Send } from "lucide-svelte";
   import { RefreshCcw } from "lucide-svelte";
 
   // Setup socket.io client
   import { io } from "socket.io-client";
   import { VtubeSocket } from "$lib";
-  import Keywords from "$lib/components/ui/keywords/Keywords.svelte";
   import { onMount } from "svelte";
   const socket = io("localhost:8080");
   socket.on("error", (data) => {
@@ -19,6 +21,8 @@
   let expressions: string[] = [];
   let vtubeSocket: VtubeSocket;
   let modelStats: any = null;
+  let bindedExpressions: any = [];
+  let expression = "";
   let vtubeAuthenticated = false;
 
   onMount(() => {
@@ -44,10 +48,16 @@
   });
 
   socket.on("current_message", (data: string) => {
-    // for (const hotkey of hotkeys) {
-    //   if (data.toLocaleLowerCase().includes(hotkey)) {
-    //   }
-    // }
+    if (!vtubeAuthenticated) return;
+
+    for (const binded of bindedExpressions) {
+      if (data.toLocaleLowerCase().includes(binded.expression)) {
+        console.log("detected binding");
+        vtubeSocket.vtubeMessage("HotkeyTriggerRequest", {
+          hotkeyID: binded.hotkeyID,
+        });
+      }
+    }
   });
 
   function retrieveCurrentModel() {
@@ -64,10 +74,21 @@
     }
   }
 
-  function addExpression(event: CustomEvent<any>) {
-    if (!expressions.includes(event.detail.expression)) {
-      expressions = [...expressions, event.detail.expression];
+  function addExpression() {
+    if (!expressions.includes(expression)) {
+      expressions = [...expressions, expression];
     }
+    expression = "";
+  }
+
+  function bindExpressionHotkey(event: SubmitEvent) {
+    const data = new FormData(event.target as HTMLFormElement);
+    const hotkeyID = data.get("hotkeyID") as string;
+    const expression = data.get("expression") as string;
+
+    const hotkey = hotkeys.find((hotkey) => hotkey.hotkeyID === hotkeyID);
+
+    bindedExpressions = [...bindedExpressions, { hotkeyID, expression, name: hotkey?.name }];
   }
 
   // Send hotkey
@@ -183,6 +204,67 @@
         </div>
       </Card.Content>
     </Card.Root>
-    <Keywords on:expression={addExpression} />
+
+    <Card.Root class="w-[480px]">
+      <Card.Header>
+        <Card.Title>Keywords</Card.Title>
+        <Card.Description>Add keywords for Vtube Studio</Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <div class="flex flex-wrap gap-2.5">
+          {#each expressions as expression}
+            <Label>{expression}</Label>
+          {/each}
+        </div>
+
+        <br />
+
+        <form on:submit|preventDefault={addExpression}>
+          <Input placeholder="New Reaction" bind:value={expression} />
+          <Button type="submit" size="icon">
+            <Send class="h-4 w-4" />
+          </Button>
+        </form>
+      </Card.Content>
+    </Card.Root>
+
+    <Card.Root class="w-[480px]">
+      <Card.Header>
+        <Card.Title>Hotkey Keywords</Card.Title>
+        <Card.Description>Bind a keyword to a hotkey</Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <table>
+          <thead>
+            <th>Expression</th>
+            <th>Keyword</th>
+          </thead>
+          <tbody>
+            {#each bindedExpressions as binded}
+              <tr>
+                <td>{binded.expression}</td>
+                <td>{binded.name}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+
+        <form on:submit|preventDefault={bindExpressionHotkey}>
+          <select name="expression">
+            {#each expressions as expression}
+              <option value={expression}>{expression}</option>
+            {/each}
+          </select>
+          <select name="hotkey">
+            {#each hotkeys as hotkey}
+              <option value={hotkey.hotkeyID}>{hotkey.name}</option>
+            {/each}
+          </select>
+          <Button type="submit" size="icon">
+            <Send class="h-4 w-4" />
+          </Button>
+        </form>
+      </Card.Content>
+    </Card.Root>
   </div>
 </div>
